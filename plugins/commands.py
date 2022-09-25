8cb510a8439c11b74504bbf9a4a15a04b70eeae5
kevinnadar <jesikamaraj@gmail.com>
2022-09-25 21:24:09 +0530

    .

--- plugins/commands.py
+++ plugins/commands.py
@@ -407,11 +407,11 @@ async def balance_cmd_handler(bot, message: Message):
             return await message.reply_text("**Please Add Password First**", quote=True)
 
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN", "/root/.apt/usr/bin/google-chrome")
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN", "/app/.apt/usr/bin/google-chrome")
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
-        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH", "/root/.chromedriver/bin/chromedriver"), chrome_options=chrome_options)
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH", "/app/.chromedriver/bin/chromedriver"), chrome_options=chrome_options)
         fetch = await message.reply_text("**🔍 Fetching Details....**\n**🚫 Don't Spam**", quote=True)
         login2 = f"https://{user['base_site']}/auth/signin"
         # driver = webdriver.Chrome()
