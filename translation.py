ea5538e49e121342b0cec3bd9e48335adc650653
Kousthubhbhat <93434418+Kousthubhbhat@users.noreply.github.com>
2022-11-03 17:21:51 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -121,7 +121,7 @@ START_MESSAGE_KEYBOARD = ReplyKeyboardMarkup([
     [KeyboardButton(text="Header"), KeyboardButton(text="Footer")],
     [KeyboardButton(text="Username"), KeyboardButton(text="Hashtag")],
     [KeyboardButton(text="Channel Link"), KeyboardButton(text="Banner Image")],
-    [KeyboardButton(text="Features"), KeyboardButton(text="⚙️Settings")],
+    [KeyboardButton(text="Features"), KeyboardButton(text="⚙️ Settings")],
     [KeyboardButton(text="Balance"), KeyboardButton(text="Account")],
     [KeyboardButton(text="Direct Download Link"), KeyboardButton(text="Stream Link")],
     [KeyboardButton(text="File Store Link"), KeyboardButton(text="Bypass")],
