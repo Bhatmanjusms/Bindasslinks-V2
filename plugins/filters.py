fbba69acdfc9c0b1294b34128d579f4f51b85dd6
Kousthubhbhat <93434418+Kousthubhbhat@users.noreply.github.com>
2022-11-08 21:54:39 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -10,10 +10,7 @@ def maintenence_mode(func):
     async def wrapper(client: "Client", message: "Message"):
 
         if Config.MAINTENENCE_MODE and message.from_user.id not in Config.ADMINS:
-            return await message.reply_text("**🛠 Guys Bot Under Maintenance 🛠
-⚙ Developers Are Working On Bot
-
-✅ When Bot Will Be Ready We Will Inform You**", quote=True)
+            return await message.reply_text("**🛠 Guys Bot Is Under Maintenance 🛠\n\n⚙ Developers Are Working On Bot ⚙\n\n✅ When Bot Will Be Ready We Will Inform You**", quote=True)
             
         return await func(client, message)
     return wrapper
